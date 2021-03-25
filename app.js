be703f6e7504063456d3844506fca8d39d781dde
Abotaleb <ezattabotaleb11@gmail.com>
2021-03-25 20:41:51 +0200

    Level 2 of security & Authentication : Mongoose encryption

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-
+const encrypt = require("mongoose-encryption");
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true , useUnifiedTopology:true});
- const userSchema = {
+ const userSchema = new mongoose.Schema ({
      email : String,
      password : String
- };
+ });
+ const secret = "Thisisourlittlesecret";
+ userSchema.plugin(encrypt, {secret : secret , encryptedFields: ['password'] })
  const User = mongoose.model("User", userSchema);
 // app instance
 
